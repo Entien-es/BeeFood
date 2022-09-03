a3a9f2f84c4111d40777c19f02ecbadd9d387dfe
namt2103 <107853376+namt2103@users.noreply.github.com>
2022-09-03 23:39:33 +0700

    update

--- data/js/coming.js
+++ data/js/coming.js
@@ -1,7 +1,8 @@
-const test = document.querySelectorAll("#footer a,#footer button");
+const test = document.querySelectorAll(
+  "#footer a,#footer button, #Download a, #Download button, #sec1 button"
+);
 test.forEach((item) => item.addEventListener("click", messageComingSoon));
 
 function messageComingSoon() {
-    alert("Coming soon");
+  alert("Coming soon");
 }
-
